7c12d04790dbc12e8bcdcf181b454ca635dee2aa
Jiameng <jmfan@bu.edu>
2020-01-22 16:41:46 -0500

    replace the outputs folder

--- ReachNN/example.sh
+++ ReachNN/example.sh
@@ -3,5 +3,5 @@ ERROR=$2
 
 cd Bernstein_Polynomial_Approximation &&\
 make name=$NNCS &&\
-python error_analysis.py --filename $NNCS --error_bound $ERROR &&
+python error_analysis.py --filename $NNCS --error_bound $ERROR &&\
 mv outputs ../
