5a9e9177b6646b43eb8b76a1ae9ab3da63c4f451
Jiameng <jmfan@bu.edu>
2020-01-22 18:03:22 -0500

    run exap

--- ReachNN/run_exp.sh
+++ ReachNN/run_exp.sh
@@ -5,16 +5,16 @@
 ./example.sh nn_1_relu_tanh 5e-3 &&\
 
 # Example 2
-./example.sh nn_12_relu 1e-2 &&\
-./example.sh nn_12_sigmoid 1e-3 &&\
-./example.sh nn_12_tanh 1e-2 &&\
-./example.sh nn_12_relu_tanh 1e-2 &&\
+./example.sh nn_2_relu 1e-2 &&\
+./example.sh nn_2_sigmoid 1e-3 &&\
+./example.sh nn_2_tanh 1e-2 &&\
+./example.sh nn_2_relu_tanh 1e-2 &&\
 
 # Example 3
-./example.sh nn_14_relu 1e-2 &&\
-./example.sh nn_14_sigmoid 5e-3 &&\
-./example.sh nn_14_tanh 1e-3 &&\
-./example.sh nn_14_relu_sigmoid 5e-3 &&\
+./example.sh nn_3_relu 1e-2 &&\
+./example.sh nn_3_sigmoid 5e-3 &&\
+./example.sh nn_3_tanh 1e-3 &&\
+./example.sh nn_3_relu_sigmoid 5e-3 &&\
 
 # Example 4
 ./example.sh nn_16_relu 5e-3 &&\
