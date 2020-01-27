6e7b61405f63019bee29f2300b5ac91cf66e5b47
Jiameng <jmfan@bu.edu>
2020-01-26 23:36:22 -0500

    new 2 tanh

--- VF_retraining/run_distillation.sh
+++ VF_retraining/run_distillation.sh
@@ -1,5 +1,7 @@
 # Example Usage (Example 1)
 ./example.sh nn_1_tanh nn_1_tanh_retrained TANH 0 4e-1 &&\
-cp nn_retrained/nn_1_tanh_retrained ../ReachNN/Bernstein_Polynomial_Approximation/nn/ &&\
+./example.sh nn_2_tanh nn_2_tanh_retrained TANH 0 1e-1 &&\
+cp nn_retrained/* ../ReachNN/Bernstein_Polynomial_Approximation/nn/ &&\
 cd ../ReachNN/ &&\
 ./example.sh nn_1_tanh_retrained 5e-3 &&\
+./example.sh nn_2_tanh_retrained 1e-2 &&\
