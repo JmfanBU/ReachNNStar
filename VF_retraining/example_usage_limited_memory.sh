9137edeb436d2189e99494912db34b7885e96628
Jiameng <jmfan@bu.edu>
2020-01-28 17:51:25 -0500

    example usage limited memory

---
+++ VF_retraining/example_usage_limited_memory.sh
@@ -0,0 +1,6 @@
+# Example Usage (Example 1)
+cd ../VF_retraining &&\
+./example.sh nn_1_relu_tanh_origin nn_1_relu_tanh_retrained RELU_TANH 0 4e-1 4 0 &&\
+cp nn_retrained/* ../ReachNN/Bernstein_Polynomial_Approximation/nn/ &&\
+cd ../ReachNN/ &&\
+./example.sh nn_1_relu_tanh_retrained 5e-3
