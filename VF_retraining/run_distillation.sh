0a3aeb26d18c2c64201974a5092136d341cbc011
Jiameng <jmfan@bu.edu>
2020-03-12 13:39:23 -0400

    fix bug in vf retraining

--- VF_retraining/run_distillation.sh
+++ VF_retraining/run_distillation.sh
@@ -3,7 +3,7 @@
 # Example 2 KD tanh
 ./example.sh nn_2_tanh nn_2_tanh_retrained TANH 0 2e-1 4 0 &&\
 # Example 6 KD, this example have different input dimension and sampling range, so example_dim_range.sh is used here.
-./example_dim_range.sh nn_tora_relu nn_tora_relu_retrained RELU 0 4e-1 0 10 4 1.5 &&\
+./example_dim_range.sh nn_tora_relu nn_tora_relu_retrained RELU 0 4e-1 1 10 4 1.5 &&\
 ./example_dim_range.sh nn_tora_relu_tanh nn_tora_relu_tanh_retrained RELU_TANH 0 4e-1 11 0 4 1.5 &&\
 
 # copy networks to reachability analysis module and rerun rechability analysis
